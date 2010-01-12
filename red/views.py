c161c5b30ae1a60103f3c205d9f9996803a4a89a
Josh Bohde <josh.bohde@gmail.com>
2010-01-11 19:15:50 -0600

    Added some base templates and CSS.

--- red/views.py
+++ red/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the poll index.")
+    return render_to_response('index.html', {"title":"Start"})
 
--- urls.py
+++ urls.py
@@ -1,4 +1,7 @@
+import settings
+
 from django.conf.urls.defaults import *
+
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -15,3 +18,9 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT})
+    )
+
