06633391700d9d657991d25cf1d5cc96e9f3a61a
Josh Bohde <josh.bohde@gmail.com>
2010-01-12 20:54:48 -0600

    Added contact info

--- red/urls.py
+++ red/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
 
 import views
 
@@ -10,4 +11,5 @@ urlpatterns = patterns('',
     url(r'^matrix/(\d+)/(\w+)/$', views.display_matrix_functions, name="red-display-functions"),
     url(r'^matrix/(\d+)/(\w+)/fever/$', views.run_fever_report, name="red-fever-report"),
     url(r'^matrix/(\d+)/(\w+)/fever/report\.txt$', views.run_text_report, name="red-text-report"),
+    url(r'^contact/$', direct_to_template, {'template': 'contact.html' }, name="red-contact"),
 )
